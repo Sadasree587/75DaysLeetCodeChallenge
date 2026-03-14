765da751b2721579a27ff19af23e0ebde333a470
Sadasree587 <sadasadasree@gmail.com>
2026-03-14 21:39:40 +0530

    Time: 7 ms (19.49%), Space: 9.7 MB (73.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,18 +5,9 @@ public:
         if (s.length() != t.length())
             return false;
 
-        int count[26] = {0};
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0)
-                return false;
-        }
-
-        return true;
+        return s == t;
     }
 };
